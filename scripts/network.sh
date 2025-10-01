80da75e27d211b1c258c7daf2237d5c424c3f05e
Titouan Delion--Desrochers <titouan.delion-desrochers@etu.univ-nantes.fr>
2025-10-01 22:02:53 +0200

    feat: modification du script d'ajouts de container en réseau

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-network=ci_network
-check_network=$(docker network ls | grep $network)
+# Variables par défaut
+network=""
+sgbdContainer=""
 
-if [ -n "$check_network" ]; then
-    echo "Le réseau ci_network existe déjà."
-    exit 0
+while getopts "d:n:" opt; do
+  case $opt in
+    d) sgbdContainer=$OPTARG ;; # -d argument
+    n) network=$OPTARG ;;       # -n argument
+    \?) echo "Usage: $0 [-d container Postgresql] [-n reseau docker]" >&2; exit 1 ;;
+  esac
+done
+
+# Vérifier l'existence du réseau
+if docker network ls --format '{{.Name}}' | grep -qx "$network"; then
+  echo "Le réseau $network existe déjà"
+else
+  echo "Le réseau $network n'existe pas. Création..."
+  docker network create "$network"
+  echo "Réseau $network créé."
 fi
 
-echo "Création du réseau ci_network"
+# Connecter le container si précisé
+if [ -n "$sgbdContainer" ]; then
+  echo "Ajout du conteneur $sgbdContainer au réseau $network"
+  docker network connect "$network" "$sgbdContainer"
+fi
 
-docker network create $network
-docker network connect $network postgresql-postgres-1 
-docker network connect $network jenkins-blueocean jenkins-docker 
-docker network connect $network jenkins-blueocean 
-docker network connect $network jenkins-docker 
