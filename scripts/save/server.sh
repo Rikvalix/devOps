ca4d2d11cd515840a5f29a2fb932a08de738c999
Titouan Delion--Desrochers <titouan.delion-desrochers@etu.univ-nantes.fr>
2025-10-02 22:09:03 +0200

    feat: script de sauvegarde des volumes avec rotation de 14 jours

---
+++ scripts/save/server.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# .env
+set -a # mode allexport 
+source .env
+set +a # désactivation du mode allexport
+
+
+BACKUP_DIR="$HOME/srv/docker-backups"
+DATE=$(date +%F)
+KEEP_DAYS=14
+
+echo "Lancement du programme de sauvegarde"
+echo "Dossier: $BACKUP_DIR"
+echo "Date: $DATE"
+echo "Rotation: $KEEP_DAYS"
+
+mkdir -p "$BACKUP_DIR/$DATE" # Créer le dossier dans le /home de l'utilisateur
+
+# Sauvegarde des volumes
+for vol in $VOLUMES; do
+	echo "Sauvegarde volume $vol ..."
+	docker run --rm \
+		-v $vol:/source \
+		-v "$BACKUP_DIR/$DATE":/backup \
+		alpine \
+		tar czf /backup/${vol}.tar.gz -C /source .
+done
+echo "Fin de sauvegarde"
+
+echo "Rotation..."
+find "$BACKUP_DIR" -maxdepth 1 -type d -mtime +$KEEP_DAYS -exec rm -rf {} \;
+echo "Fin de rotation"
+
+echo "Envoi de notification"
+# Envoi de la notification
+curl -H "Content-Type: application/json" \
+     -X POST \
+     -d "{
+       \"embeds\": [{
+         \"title\": \"Sauvegarde volumes Docker\",
+         \"description\": \"Sauvegarde terminée\",
+         \"color\": 3066993,
+         \"footer\": {\"text\": \"$DATE\"}
+       }]
+     }" \
+     "$WEBHOOK_URL"
