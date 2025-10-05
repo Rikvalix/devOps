5f2b37baafe6bffd711cc1d2ecc76eae71c1d411
Titouan Delion--Desrochers <titouan.delion-desrochers@etu.univ-nantes.fr>
2025-10-05 13:51:36 +0200

    feat: script de sauvegarde côté client

--- ci-cd/scripts/save/client.sh
+++ ci-cd/scripts/save/client.sh
@@ -3,11 +3,12 @@
 # Script de récupération des sauvegardes depuis le serveur de sauvegarde
 
 
-.env SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-echo $SCRIPT_DIR 
+#.env
+ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 set -a # mode allexport 
 source $SCRIPT_DIR/config.env 
 set +a # désactivation du mode allexport 
 
+mkdir -p $DESTINATION_PATH # S'assurer que le dossier est crée 
 
 rsync -avP --delete "$SSH_SOURCE:$SOURCE_PATH" "$DESTINATION_PATH"
