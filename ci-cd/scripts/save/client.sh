1d326b314981aeece2f1623954ea909ed1b666a9
Titouan Delion--Desrochers <titouan.delion-desrochers@etu.univ-nantes.fr>
2025-10-05 13:36:03 +0200

    feat: V1 script client.sh

--- ci-cd/scripts/save/client.sh
+++ ci-cd/scripts/save/client.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 # Script de récupération des sauvegardes depuis le serveur de sauvegarde
+
+
+.env SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+echo $SCRIPT_DIR 
+set -a # mode allexport 
+source $SCRIPT_DIR/config.env 
+set +a # désactivation du mode allexport 
+
+
+rsync -avP --delete "$SSH_SOURCE:$SOURCE_PATH" "$DESTINATION_PATH"
