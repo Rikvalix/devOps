bdf4b17120405f1cd8e86c358363dfdf0da06ecb
Titouan Delion--Desrochers <titouan.delion-desrochers@etu.univ-nantes.fr>
2025-10-05 12:54:48 +0200

    feat: initialisation d'environnement, parti ktor initialisé

--- app/scripts/create_environment.sh
+++ app/scripts/create_environment.sh
@@ -4,31 +4,69 @@
 # Auteur: Titouan DELION--DESROCHERS
 # Date: 2025-10-05
 
+# Chargement utils
+source "$(dirname "$0")/utils/common.sh"
 
 # Script de création d'un environnement de développement
-
 ENV_NAME=""
+REGISTRY=""
+BUILDER_NAME=""
+SOURCE_NAME="ktor-source"
+KTOR_PORT=""
 
-while getopts "n:" opt; do
+IMAGE="$REGISTRY/$BUILDER_NAME"
+
+while getopts "n:r:b:p:" opt; do
   case $opt in
     n) ENV_NAME=$OPTARG ;; # -n argument
-    \?) echo "Usage: $0 [-n nom_de_l'environnement]" >&2; exit 1 ;;
+    r) REGISTRY=$OPTARG ;; # -r argument
+    b) BUILDER_NAME=$OPTARG ;; #-b argument
+    p) KTOR_PORT=$OPTARG ;;
+    \?) echo "Usage: $0 [-n nom_de_l'environnement] [-r adresse du registry] [-b nom donnée au builder] [-p ktor port] " >&2; exit 1 ;;
   esac
 done
+
+if [ -z "$ENV_NAME" ]; then
+    	log_error "L'argument -n (nom de l'environnement) est obligatoire"
+    	exit 1
+fi
+
+if [ -z "$REGISTRY" ]; then
+    	log_error "L'argument -r (adresse du registry) est obligatoire"
+    	exit 1
+fi
+
+if [ -z "$BUILDER_NAME" ]; then
+    	log_error "L'argument -b (nom du builder) est obligatoire"
+    	exit 1
+fi
+
+if [ -z "$KTOR_PORT" ]; then
+	log_error "L'argument -p (port ktor sur le réseau host) est obligatoire"
+	exit 1
+fi
 
 
 # Création du network
 if docker network ls --format '{{.Name}}' | grep -qx "$ENV_NAME"; then
-  echo "Le réseau $ENV_NAME existe déjà"
+  log_info "Le réseau $ENV_NAME existe déjà"
 else
-  echo "Le réseau $ENV_NAME n'existe pas. Création..."
+  log_info "Le réseau $ENV_NAME n'existe pas. Création..."
   docker network create "$ENV_NAME"
-  echo "Réseau $ENV_NAME créé."
+  log_info "Réseau $ENV_NAME créé."
 fi
-
-# Création du container PostgreSQL
 
 # Création du container Ktor
 
-# Vérifier si le builder est dans le registry
+bash $(dirname "$0")/init_ktor_source.sh -r $REGISTRY -b $BUILDER_NAME -s $SOURCE_NAME
+
+log_info "Création et démarrage du container $SOURCE_NAME..."
+
+docker run -d \
+  --name "$SOURCE_NAME-$ENV_NAME" \
+  --network "$ENV_NAME" \
+  -p "127.0.0.1:$KTOR_PORT:8080" \
+  "$SOURCE_NAME"
+
+log_info "Container $SOURCE_NAME démarré, sur 127.0.0.1:$KTOR_PORT"
 
---
+++ app/scripts/init_ktor_source.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Nom:  init_ktor_source.sh
+# Objet: Créer l'image source de ktor et le push sur le registry
+# Auteur: Titouan DELION--DESROCHERS
+# Date: 2025-10-05
+
+# Chargement utils
+source "$(dirname "$0")/utils/common.sh"
+
+# Variables par défaut
+REGISTRY=""
+BUILDER_NAME=""
+SOURCE_NAME="" # Nom de l'image final
+
+while getopts "r:b:s:" opt; do
+	case $opt in
+	  r) REGISTRY=$OPTARG ;; # -r registry
+	  b) BUILDER_NAME=$OPTARG ;; # -b builder name
+	  s) SOURCE_NAME=$OPTARG ;; # -s image final
+	  \?) echo "Usage [-r adresse du registry] [-b nom donnée au builder] [-s nom donnée à l'image final] " >&2; exit 1 ;;
+	esac
+done
+
+IMAGE="$REGISTRY/$BUILDER_NAME"
+
+if [ -z "$REGISTRY" ]; then
+	log_error "L'argument -r est obligatoire"
+	exit 1
+fi
+
+if [ -z "$BUILDER_NAME" ]; then 
+	log_error "L'argument -b est obligatoire"      
+	exit 1
+fi
+
+if [ -z "$SOURCE_NAME" ]; then
+	log_error "L'argument -s est obligatoire"
+	exit 1
+fi
+
+log_info "Initialisation source Ktor"
+log_info "Image: $IMAGE"
+log_info "Root dir: $ROOT_DIR"
+log_info "Image final: $SOURCE_NAME"
+echo "---"
+
+log_info "Tentative de pull l'image $IMAGE ..."
+
+if docker pull "$IMAGE"; then
+    log_info "Image $IMAGE trouvée sur le registry."
+else
+    log_info "Image $IMAGE introuvable, construction en cours..."
+    bash $(dirname "$0")/init_ktor_builder.sh -r $REGISTRY -b $BUILDER_NAME
+    # On ne pull pas car lors de sa construction l'image est stocké
+fi
+
+
+# Crée l'image source
+log_info "Build de l'image $SOURCE_NAME..."
+docker build -t "$SOURCE_NAME" -f "$ROOT_DIR/ktor/source/Dockerfile" "$ROOT_DIR/ktor/source" 
+docker images "$SOURCE_NAME"
+
